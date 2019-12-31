17174dacfb27fd9977d000df078324b515392b88
VinithaRajendran-3005 <viniraj95@gmail.com>
2019-12-31 17:36:40 +0530

    1 file modified

--- components/About.js
+++ components/About.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 
-class About extends Component {
-  render(){
-    return(
-      <div>
-        <p>About</p>
-      </div>
-    )
+class About extends React.Component {
+  constructor() {
+    this.state = {
+      error: null,
+      Loaded: false,
+      albums: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/todos/")
+      .then(response => response.json())
+      .then(albums => { this.setState({ Loaded: true, albums }) },
+      error => { this.setState({ Loaded: true, error }); })
+  }
+
+  render() {
+    const { error, Loaded, albums } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!Loaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {albums.map(album => (<li key={album.id}>{album.title}</li>))}
+        </ul>
+      );
+    }
   }
 }
 
 export default About
+
+
+  
